93a32241087d29b64bcacde0dc1a5215b00b716b
olinicker <linicker.ostroski@alunos.ifsuldeminas.edu.br>
2026-05-18 19:45:31 -0300

    feat: cria diretórios dentro de src

---
+++ bgcloset-app/src/services/api.ts
@@ -0,0 +1,24 @@
+import axios, { type InternalAxiosRequestConfig } from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000/api",
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    const token = localStorage.getItem("@BG_Token");
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default api;
